b33a3c8b9bd695d5e82286f62cd3dc5057550eb5
alagibinsani <48465507+alagibinsani@users.noreply.github.com>
2021-04-13 15:30:17 +0700

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan krywn = new karyawan();
+
+            krywn.nama = "Agib";
+            krywn.nik = "2380";
+            krywn.gaji = 1000000;
+
+            krywn.nama2 = "Lako";
+            krywn.nik2 = "2374";
+            krywn.gaji2 = 2000000;
+
+            krywn.infoGaji();
+            Console.WriteLine();
+            krywn.kenaikanGaji();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
